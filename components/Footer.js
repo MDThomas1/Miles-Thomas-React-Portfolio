07747f1a464d29d4add8d9dbb331c0fca3b8f46b
Miles Thomas <milesman2013@gmail.com>
2023-01-09 17:51:00 +1100

    header and footer content

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div className='footer'>
+            <a href=''>Github</a>
+            <a href=''>Instagram</a>
+            <a href=''>LinkedIn</a>
+        </div>
+    );
+}

fe48bb73d3c3e72efc4388e6aa90cfb94b65f308
Miles Thomas <milesman2013@gmail.com>
2023-01-09 17:25:42 +1100

    navbar with functions

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,1 +1,23 @@
+import React from 'react';
+
+function Navigation({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a href="About-Me" onClick={() => handlePageChange('About-Me')}>Home</a>
+      </li>
+      <li className="nav-item">
+        <a href="#Portfolio" onClick={() => handlePageChange('Portfolio')}>About</a>
+      </li>
+      <li className="nav-item">
+        <a href="#Contact" onClick={() => handlePageChange('Contact')}>Blog</a>
+      </li>
+      <li className="nav-item">
+        <a href="#Resume" onClick={() => handlePageChange('Resume')}>Contact</a>
+      </li>
+    </ul>
+  );
+}
+
+export default Navigation
 

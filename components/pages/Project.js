d4fe9df852c04fdee7c91cfc623b789a7cb36495
Miles Thomas <milesman2013@gmail.com>
2023-01-12 14:45:31 +1100

    project card final prop

--- components/pages/Project.js
+++ components/pages/Project.js
@@ -4,7 +4,9 @@ export default function Project(props) {
     return (
         <div className='project'>
             <h3>{props.name}</h3>
-            <img src={`${props.image}`}></img>
+            <a href={`${props.link}`}>
+                <img src={`${props.image}`}></img>
+            </a>
             <p>{props.description}</p>
         </div>
     );

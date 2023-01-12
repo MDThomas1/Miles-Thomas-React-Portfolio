2f96010d8f499496aa1aa01611a33d9616581ccc
Miles Thomas <milesman2013@gmail.com>
2023-01-12 17:23:54 +1100

    nav link class change

--- miles-thomas-react-portfolio/src/components/Navigation.js
+++ miles-thomas-react-portfolio/src/components/Navigation.js
@@ -4,16 +4,16 @@ function Navigation({ currentPage, handlePageChange }) {
   return (
     <ul className="nav nav-tabs">
       <li className="nav-item">
-        <a href="About-Me" onClick={() => handlePageChange('About-Me')}>Home</a>
+        <a href="About-Me" onClick={() => handlePageChange('About-Me')} className={currentPage === 'About-Me' ? 'nav-link current' : 'nav-link'}>About Me</a>
       </li>
       <li className="nav-item">
-        <a href="#Portfolio" onClick={() => handlePageChange('Portfolio')}>About</a>
+        <a href="#Portfolio" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link current' : 'nav-link'}>Portfolio</a>
       </li>
       <li className="nav-item">
-        <a href="#Contact" onClick={() => handlePageChange('Contact')}>Blog</a>
+        <a href="#Contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link current' : 'nav-link'}>Contact</a>
       </li>
       <li className="nav-item">
-        <a href="#Resume" onClick={() => handlePageChange('Resume')}>Contact</a>
+        <a href="#Resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link current' : 'nav-link'}>Resumé</a>
       </li>
     </ul>
   );

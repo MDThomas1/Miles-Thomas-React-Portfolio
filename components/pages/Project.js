e25a5a649e40498186ca7f62051a40bf6b8de764
Miles Thomas <milesman2013@gmail.com>
2023-01-12 14:14:00 +1100

    project cards

--- components/pages/Project.js
+++ components/pages/Project.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function Project() {
+export default function Project(props) {
     return (
         <div className='project'>
-
+            <h3>{props.name}</h3>
+            <img src=''></img>
+            <p>{props.description}</p>
         </div>
     );
 }

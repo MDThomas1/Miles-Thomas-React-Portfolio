33da51e48e5fd24ac36260bb4e19670993c82e14
Miles Thomas <milesman2013@gmail.com>
2023-01-12 16:47:15 +1100

    contact cleanup

--- miles-thomas-react-portfolio/src/components/pages/Contact.js
+++ miles-thomas-react-portfolio/src/components/pages/Contact.js
@@ -4,8 +4,8 @@ export default function Contact() {
     return (
         <div className='contact'>
             <h2>Contact</h2>
-            <input type={'text'} placeholder={'Please enter your name'}></input>
-            <input type={'text'} placeholder={'Please enter your email address'}></input>
+            <input type='text' placeholder='Please enter your name'></input>
+            <input type='text' placeholder='Please enter your email address'></input>
             <textarea></textarea>
             <button>Submit</button>
         </div>

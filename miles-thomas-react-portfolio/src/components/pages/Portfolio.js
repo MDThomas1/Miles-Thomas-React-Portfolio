ce3af840cfc03f50c6f9ed791b9b2e954ef6695a
Miles Thomas <milesman2013@gmail.com>
2023-01-12 16:14:47 +1100

    portfolio page

--- miles-thomas-react-portfolio/src/components/pages/Portfolio.js
+++ miles-thomas-react-portfolio/src/components/pages/Portfolio.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-export default function Portfolio() {
+import Project from './Project'
+
+export default function Portfolio({ projects }) {
     return (
         <div className='portfolio'>
-
+            <h2>Portfolio</h2>
+            {projects.map((project) => {
+                <Project name={project.name} link={project.link} image={project.image} description={project.description} github={project.github} />
+            })}
         </div>
     );
 }

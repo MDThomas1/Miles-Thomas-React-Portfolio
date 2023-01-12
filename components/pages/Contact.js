8536024dbbc136bf3d368cff3103080d66ec0e5f
Miles Thomas <milesman2013@gmail.com>
2023-01-12 14:19:47 +1100

    contact page elements

--- components/pages/Contact.js
+++ components/pages/Contact.js
@@ -3,6 +3,10 @@ import React from 'react';
 export default function Contact() {
     return (
         <div className='contact'>
+            <input type={'text'} placeholder={'Please enter your name'}></input>
+            <input type={'text'} placeholder={'Please enter your name'}></input>
+            <input type={'text'} placeholder={'Please enter your name'}></input>
+            <button>Submit</button>
         </div>
     );
 }

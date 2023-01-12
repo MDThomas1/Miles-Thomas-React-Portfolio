acb04e0578e8054fab14a67705a9e2656b64692b
Miles Thomas <milesman2013@gmail.com>
2023-01-12 20:49:20 +1100

    forgot to save change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './reset.css';
-import 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
 import './style.css';
 import App from './App';
 

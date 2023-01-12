1497cfa090c3334187a0c6a8fd496cd89c1f8857
Miles Thomas <milesman2013@gmail.com>
2023-01-12 15:31:39 +1100

    more clean-up

--- components/pages/Contact.js
+++ components/pages/Contact.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Contact() {
     return (
         <div className='contact'>
+            <h2>Contact</h2>
             <input type={'text'} placeholder={'Please enter your name'}></input>
             <input type={'text'} placeholder={'Please enter your email address'}></input>
             <input type={'text'} placeholder={'Please enter your message'}></input>

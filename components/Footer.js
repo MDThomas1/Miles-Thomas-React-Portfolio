d6419cdcc1071beea77a14088ed6a72111dbb1a5
Miles Thomas <milesman2013@gmail.com>
2023-01-12 13:20:11 +1100

    footer update

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Footer() {
     return (
         <div className='footer'>
-            <a href=''>Github</a>
-            <a href=''>Instagram</a>
+            <a href='https://github.com/MDThomas1'>Github</a>
+            <a href='https://www.instagram.com/groovyboi1/'>Instagram</a>
             <a href=''>LinkedIn</a>
         </div>
     );

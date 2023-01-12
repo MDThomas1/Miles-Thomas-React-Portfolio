20faf80683b9de86321300d73b4ef433933ca324
Miles Thomas <milesman2013@gmail.com>
2023-01-12 20:44:34 +1100

    bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './reset.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
 import './style.css';
 import App from './App';
 

0a2bd4cace579c3302cd793f4db14d792d3eedc5
Miles Thomas <milesman2013@gmail.com>
2023-01-12 14:48:50 +1100

    text cleanup

--- components/pages/Contact.js
+++ components/pages/Contact.js
@@ -4,8 +4,8 @@ export default function Contact() {
     return (
         <div className='contact'>
             <input type={'text'} placeholder={'Please enter your name'}></input>
-            <input type={'text'} placeholder={'Please enter your name'}></input>
-            <input type={'text'} placeholder={'Please enter your name'}></input>
+            <input type={'text'} placeholder={'Please enter your email address'}></input>
+            <input type={'text'} placeholder={'Please enter your message'}></input>
             <button>Submit</button>
         </div>
     );
